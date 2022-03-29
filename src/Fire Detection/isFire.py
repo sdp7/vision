432fcba2165def79079d436e5e4107773fc199e0
arkaban <arka.purkayastha10@gmail.com>
2022-03-29 08:10:45 +0100

    isFire being debugged

--- src/Fire Detection/isFire.py
+++ src/Fire Detection/isFire.py
@@ -5,20 +5,21 @@ from std_msgs.msg import Float64MultiArray
 import cv2
 import numpy as np
 import imutils
+
 class FireDetector:
     def __init__(self):
         self.capture = cv2.VideoCapture(0)
 
         rospy.init_node('fire_location')
         self.rate = rospy.Rate(10)
-        self.pub = rospy.Publisher("fire_location", Float64MultiArray, queue_size=10)
-        net = cv2.dnn.readNet("best.onnx")
-        cap = cv2.VideoCapture(0)
-        colors = [(255, 255, 0), (0, 255, 0), (0, 255, 255), (255, 0, 0)]
+        self.pub = rospy.Publisher("is_Fire", Float64MultiArray, queue_size=10)
+        self.net = cv2.dnn.readNet("best.onnx")
+        self.cap = cv2.VideoCapture(0)
+        self.colors = [(255, 255, 0), (0, 255, 0), (0, 255, 255), (255, 0, 0)]
         self.counter = 0
 
     #Find Fires    
-    def unwrap_detection(input_image, output_data):
+    def unwrap_detection(self,input_image, output_data):
         class_ids = []
         confidences = []
         boxes = []
@@ -53,8 +54,9 @@ class FireDetector:
                     boxes.append(box)
 
         return class_ids, confidences, boxes
+    
     # Format the frame
-    def format_yolov5(source):
+    def format_yolov5(self, source):
         #print(source.shape)	
         # put the image in square big enough
         col, row, _ = source.shape
@@ -64,24 +66,22 @@ class FireDetector:
     
     # resize to 640x640, normalize to [0,1[ and swap Red and Blue channels
         result = cv2.dnn.blobFromImage(resized, 1/255.0, (640, 640), swapRB=True)
-    
         return result
 
-
-
-# the output will be written to output.avi
-#out = cv2.VideoWriter(
- #   'output.avi',
-  #  cv2.VideoWriter_fourcc(*'MJPG'),
-   # 15.,
-    #(640,640))
-   #This should publish the coordinates of the fire (specifically the base of the fire)
+    # the output will be written to output.avi
+    # out = cv2.VideoWriter(
+    # 'output.avi',
+    # cv2.VideoWriter_fourcc(*'MJPG'),
+    # 15.,
+    # (640,640))
+    
+    #This should publish the coordinates of the fire (specifically the base of the fire)
     def publish(self):
-        ret,frame = cap.read()
-        shapedframe = format_yolov5(frame)
-        net.setInput(shapedframe)
-        outs = net.forward()
-        class_ids, confidences, boxes = unwrap_detection(shapedframe, outs[0])
+        ret,frame = self.cap.read()
+        shapedframe = self.format_yolov5(frame)
+        self.net.setInput(shapedframe)
+        outs = self.net.forward()
+        class_ids, confidences, boxes = self.unwrap_detection(shapedframe, outs[0])
         mainFire = np.argmax(confidences)
         mainFireLoc = boxes[mainFire]
         CentreX = mainFireLoc[0] + mainFireLoc[2]
@@ -89,16 +89,18 @@ class FireDetector:
         CoordMsg = Float64MultiArray()
         CoordMsg.data.append(CentreX)
         CoordMsg.data.append(CentreY)
-        self.pub.publish(dCoordMsg)
+        self.pub.publish(CoordMsg)
         self.rate.sleep()
-	 #For Printing out Fire
-	#for (classid, confidence, box) in zip(class_ids, confidences, boxes):
-         #	color = colors[int(classid) % len(colors)]
-         #	cv2.rectangle(frame, box, color, 2)
-         #	cv2.rectangle(frame, (box[0], box[1] - 20), (box[0] + box[2], box[1]), color, -1)
-         #	cv2.putText(frame, 'fire', (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, .5, (0,0,0))
-	#cv2.imshow("output", frame)
-	#cv2.waitKey(1)
+	
+        #For Printing out Fire
+        #for (classid, confidence, box) in zip(class_ids, confidences, boxes):
+            #	color = colors[int(classid) % len(colors)]
+            #	cv2.rectangle(frame, box, color, 2)
+            #	cv2.rectangle(frame, (box[0], box[1] - 20), (box[0] + box[2], box[1]), color, -1)
+            #	cv2.putText(frame, 'fire', (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, .5, (0,0,0))
+        #cv2.imshow("output", frame)
+        #cv2.waitKey(1)
+
 if __name__ == '__main__':
     F = FireDetector()
     while not rospy.is_shutdown():
